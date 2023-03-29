6946a7f8eb24ff8405653fcccdd28e0ace2af23e
Code Sandbox <hello@codesandbox.io>
2023-03-29 16:06:54 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d62cb6cf81b507a9c064cfa38b6d0121.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910c6347b21a37f1883a933b388ca28a",
+    "url": "https://thititj.github.io/csb-ctinjd/index.html"
+  },
+  {
+    "revision": "94f934fc707555f3a5d7",
+    "url": "https://thititj.github.io/csb-ctinjd/static/css/main.f0f593cd.chunk.css"
+  },
+  {
+    "revision": "b31c52e0242a3cd4ca54",
+    "url": "https://thititj.github.io/csb-ctinjd/static/js/2.21e0e259.chunk.js"
+  },
+  {
+    "revision": "94f934fc707555f3a5d7",
+    "url": "https://thititj.github.io/csb-ctinjd/static/js/main.78d8c128.chunk.js"
+  },
+  {
+    "revision": "edc79372cb6bac553556",
+    "url": "https://thititj.github.io/csb-ctinjd/static/js/runtime~main.621ce923.js"
+  },
+  {
+    "revision": "f4a02341cbb165c4f860da0935358362",
+    "url": "https://thititj.github.io/csb-ctinjd/static/media/ginger-cat-upgrade.f4a02341.png"
+  }
+]);
